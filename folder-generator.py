02d4a7e570b46d1003c1d939c2cac5588f9b31cb
chrisdangcode <cdangcode@gmail.com>
2019-11-03 15:59:11 -0600

    initial commit

---
+++ folder-generator.py
@@ -0,0 +1,25 @@
+# Based from: https://gist.github.com/keithweaver/562d3caa8650eefe7f84fa074e9ca949
+# Date time info: https://learnandlearn.com/python-programming/python-reference/python-get-current-date
+
+# Works for python 3.8
+
+import os
+import datetime
+
+def createFolder(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+    except OSError:
+        print ('Error: Creating directory. ' +  directory)
+        
+today = datetime.datetime.today()
+current_day = today.strftime('%Y_%m_%d')
+current_day_RAW = "./" + current_day  + "/RAW"
+current_day_selects = "./" + current_day + "/selects"
+current_day_video = "./" + current_day + "/video"
+
+createFolder(current_day)
+createFolder(current_day_RAW)
+createFolder(current_day_selects)
+createFolder(current_day_video)
